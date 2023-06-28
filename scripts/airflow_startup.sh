a7b926633d6c7b07f3f1500bb7aac7ee6fbd8966
Yuan <szuyuan25@gmail.com>
2023-06-28 20:09:43 +0200

    remove - from docker-compose command

--- scripts/airflow_startup.sh
+++ scripts/airflow_startup.sh
@@ -5,14 +5,14 @@ cd ~/DE-Project-2/ && sudo chmod -R 777 dbt
 
 echo "Building airflow docker images..."
 cd ~/DE-Project-2/airflow
-docker-compose build
+docker compose build
 
 echo "Running airflow-init..."
-docker-compose up airflow-init
+docker compose up airflow-init
 
 echo "Starting up airflow in detached mode..."
-docker-compose up -d
+docker compose up -d
 
 echo "Airflow started successfully."
 echo "Airflow is running in detached mode. "
-echo "Run 'docker-compose logs --follow' to see the logs."
+echo "Run 'docker compose logs --follow' to see the logs."

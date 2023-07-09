82c3edde679aa0032288a9a5cc296a97b88b22ff
Yuan <szuyuan25@gmail.com>
2023-07-09 11:09:00 +0200

    add fact and wide models

---
+++ dbt/models/mart/fact_streams.sql
@@ -0,0 +1,25 @@
+{{ config(materialized = 'table') }}
+
+WITH listen_events AS (
+    SELECT *
+    FROM {{ ref('stg_listen_events') }}
+)
+
+SELECT 
+    dim_users.user_key,
+    dim_artists.artist_key,
+    dim_songs.song_key,
+    dim_datetime.date_key,
+    dim_location.location_key,
+    listen_events.ts
+ FROM listen_events
+  LEFT JOIN {{ ref('dim_users') }} 
+    ON listen_events.user_id = dim_users.user_id AND CAST(listen_events.ts AS DATE) >= dim_users.row_activation_date AND CAST(listen_events.ts AS DATE) < dim_users.row_expiration_date
+  LEFT JOIN {{ ref('dim_artists') }} 
+    ON REPLACE(REPLACE(listen_events.artist, '"', ''), '\\', '') = dim_artists.name
+  LEFT JOIN {{ ref('dim_songs') }} 
+    ON REPLACE(REPLACE(listen_events.artist, '"', ''), '\\', '') = dim_songs.artist_name AND listen_events.song = dim_songs.title
+  LEFT JOIN {{ ref('dim_location') }} 
+    ON listen_events.city = dim_location.city AND listen_events.state = dim_location.state_code AND listen_events.lat = dim_location.latitude AND listen_events.lon = dim_location.longitude 
+  LEFT JOIN {{ ref('dim_datetime') }} 
+    ON dim_datetime.date = to_char(listen_events.ts, 'YYYY-MM-DD HH24:00:00.000')::timestamp
---
+++ dbt/models/mart/wide_streams.sql
@@ -0,0 +1,35 @@
+{{ config(
+    materialized = 'view'
+) }}
+SELECT 
+fact_streams.user_key,
+    fact_streams.artist_key,
+    fact_streams.song_key,
+    fact_streams.date_key,
+    fact_streams.location_key,
+    fact_streams.ts AS timestamp,
+    dim_users.first_name,
+    dim_users.last_name ,
+    dim_users.gender,
+    dim_users.level,
+    dim_users.user_id,
+    dim_users.current_row as current_user_row,
+    dim_songs.duration AS song_duration,
+    dim_songs.tempo,
+    dim_songs.title AS song_name,
+    dim_location.city,
+    dim_location.state_name AS state,
+    dim_location.latitude,
+    dim_location.longitude,
+    dim_datetime.date AS date_hour,
+    dim_datetime.day_of_month,
+    dim_datetime.day_of_week,
+    dim_artists.latitude AS artist_latitude,
+    dim_artists.longitude AS artist_longitude,
+    dim_artists.name AS artist_name
+FROM {{ ref('fact_streams') }}
+    JOIN {{ ref('dim_users') }} ON fact_streams.user_key = dim_users.user_key
+    JOIN {{ ref('dim_songs') }} ON fact_streams.song_key = dim_songs.song_key
+    JOIN {{ ref('dim_location') }} ON fact_streams.location_key = dim_location.location_key
+    JOIN {{ ref('dim_datetime') }} ON fact_streams.date_key = dim_datetime.date_key
+    JOIN {{ ref('dim_artists') }} ON fact_streams.artist_key = dim_artists.artist_key

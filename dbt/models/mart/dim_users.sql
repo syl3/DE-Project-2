1b6c9ac6be9aa42fde1b52fd66582dc89c1df898
Yuan <szuyuan25@gmail.com>
2023-07-09 11:08:24 +0200

    add dim_users

---
+++ dbt/models/mart/dim_users.sql
@@ -0,0 +1,104 @@
+{{ config (MATERIALIZED = 'table') }} 
+-- level column in the users dimension is considered to be a SCD2 change. Just for the purpose of learning to write SCD2 change queries.
+-- The below query is constructed to accommodate changing levels from free to paid and maintaining the latest state of the user along with
+-- historical record of the user's level
+WITH listen_events AS (
+    -- Select distinct state of user in each timestamp
+    SELECT DISTINCT
+        user_id,
+        first_name,
+        last_name,
+        gender,
+        registration,
+        level,
+        ts AS date
+    FROM
+        {{ ref ('stg_listen_events') }}
+    WHERE
+        user_id <> 0
+),
+listen_events__where_level_change AS (
+    -- Lag the level and see where the user changes level from free to paid or otherwise
+    SELECT
+        *,
+        CASE WHEN LAG(level, 1, 'NA') OVER (PARTITION BY user_id,
+            first_name,
+            last_name,
+            gender ORDER BY date) <> level THEN
+            1
+        ELSE
+            0
+        END AS lagged
+    FROM
+        listen_events
+),
+listen_events__group AS (
+    -- Create distinct group of each level change to identify the change in level accurately
+    SELECT
+        *,
+        SUM(lagged) OVER (PARTITION BY user_id,
+            first_name,
+            last_name,
+            gender ORDER BY date) AS grouped
+    FROM
+        listen_events__where_level_change
+),
+listen_events__find_min_date_within_group AS (
+    -- Find the earliest date available for each free/paid status change
+    SELECT
+        user_id,
+        first_name,
+        last_name,
+        gender,
+        registration,
+        level,
+        grouped,
+        cast(min(date) AS date) AS mindate
+    FROM
+        listen_events__group
+    GROUP BY
+        user_id,
+        first_name,
+        last_name,
+        gender,
+        registration,
+        level,
+        grouped
+),
+listen_events__assign_flag__activation_date__expiration_date AS (
+    SELECT
+        CAST(user_id AS bigint) AS user_id,
+        first_name,
+        last_name,
+        gender,
+        level,
+        CAST(registration AS bigint) AS registration,
+        mindate AS row_activation_date,
+        -- Choose the start date from the next record and add that as the expiration date for the current record
+        LEAD(minDate, 1, '9999-12-31') OVER (PARTITION BY user_id,
+            first_name,
+            last_name,
+            gender ORDER BY grouped) AS row_expiration_date,
+        -- Assign a flag indicating which is the latest row for easier select queries
+        CASE WHEN RANK() OVER (PARTITION BY user_id,
+            first_name,
+            last_name,
+            gender ORDER BY grouped DESC) = 1 THEN
+            1
+        ELSE
+            0
+        END AS current_row
+    FROM
+        listen_events__find_min_date_within_group
+),
+final AS (
+    SELECT
+        {{ dbt_utils.surrogate_key (['user_id', 'row_activation_date', 'level']) }} AS user_key,
+        *
+    FROM
+        listen_events__assign_flag__activation_date__expiration_date
+)
+SELECT
+    *
+FROM
+    final

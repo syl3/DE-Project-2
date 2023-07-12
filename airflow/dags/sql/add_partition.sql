778345e3e16b874d23845615526d3b5fff1c46e4
Yuan <szuyuan25@gmail.com>
2023-07-12 20:02:42 +0200

    add partition to external table

--- airflow/dags/sql/add_partition.sql
+++ airflow/dags/sql/add_partition.sql
@@ -1,2 +1,2 @@
 ALTER TABLE spectrum.{{ params.event }} ADD
-PARTITION (ts_partition={{ logical_date.strftime("%Y%m%d%H") }}) LOCATION 's3://{{params.s3_bucket_name}}/{{ params.event }}/month={{ logical_date.strftime("%-m") }}/day={{ logical_date.strftime("%-d") }}/hour={{ logical_date.strftime("%-H") }}';
+PARTITION (ts_partition='{{ logical_date.strftime("%Y%m%d%H") }}') LOCATION 's3://{{params.s3_bucket_name}}/{{ params.event }}/month={{ logical_date.strftime("%-m") }}/day={{ logical_date.strftime("%-d") }}/hour={{ logical_date.strftime("%-H") }}';
